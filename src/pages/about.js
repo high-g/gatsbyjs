c644fa51ede077c6d75420f939b7c3c42d3736a1
tani <tani@1-10.com>
2021-08-12 17:06:03 +0900

    Update emotion

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,21 @@
-import React from "react"
+import React from 'react'
+import { css } from '@emotion/react'
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div css={wrap}>
+      <h1 css={title}>About</h1>
       <p>Hello about!</p>
     </div>
   )
 }
+
+const wrap = css`
+  background: red;
+`
+
+const title = css`
+  color: blue;
+`
 
 export default About
